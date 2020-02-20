98c08611655edbca03f8ca51351360a98b5e06a4
Bryant Lee <bl692@cornell.edu>
2020-02-20 11:00:46 -0500

    increased speed of automation

--- find_the_invisible_cow.py
+++ find_the_invisible_cow.py
@@ -1,6 +1,7 @@
 import pyautogui
 import pyperclip
 import time
+import re
 
 firefox_y_offset = 100
 
@@ -10,43 +11,38 @@ def copy_clipboard():
     time.sleep(.01)  # ctrl-c is usually very fast but your program may execute faster
     return pyperclip.paste()
 
-def toggle_console():    
+def toggle_console():
     pyautogui.hotkey('command', 'option', 'i')
 
 def click_play():
     pyautogui.moveTo(100, 150)
 
 def click_cow(coordinates):
-    pyautogui.moveTo(coordinates[0], coordinates[1] + firefox_y_offset)
+    pyautogui.moveTo(int(coordinates[0]), int(coordinates[1]) + firefox_y_offset)
     pyautogui.click()
 
-def get_coordinate(plane):
-    pyautogui.typewrite('alert(window.app.children[1].__vue__._data.{})'.format(plane))
-    pyautogui.hotkey('return')
-    coordinate = copy_clipboard()
-    pyautogui.hotkey('return')
-    return int(coordinate.strip())
-
 def get_coordinates():
-    planes = ['x', 'y']
-    coordinates = []
-    for plane in planes:
-        toggle_console()
-        time.sleep(0.3)
-        coordinates.append(get_coordinate(plane))
-        toggle_console()
-    return coordinates
+    toggle_console()
+    time.sleep(0.08)
+    pyperclip.copy('alert(window.app.children[1].__vue__._data.x + ", " + window.app.children[1].__vue__._data.y)')
+    pyautogui.hotkey('command', 'v')
+    pyautogui.hotkey('return')
+    time.sleep(.01)
+    coordinates = re.findall('(\d+), (\d+)', copy_clipboard())
+    pyautogui.hotkey('return')
+    toggle_console()
+    return coordinates[0]
 
 def click_play_again():
     pyautogui.moveTo(457, 560)
     pyautogui.click()
 
 if __name__ == '__main__':
-    time.sleep(4)    
+    time.sleep(2)
     while True:
         coordinates = get_coordinates()
         click_cow(coordinates)
-        time.sleep(3)  
+        time.sleep(2.25)
         click_play_again()
-        time.sleep(1)  
+        time.sleep(0.05)
 
